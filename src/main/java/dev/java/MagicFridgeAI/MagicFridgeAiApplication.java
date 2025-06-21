0d4d1f92c316a81248968cc1e79c5c09e8044ce6
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-06-21 10:45:35 -0300

    build: create base of project

---
+++ src/main/java/dev/java/MagicFridgeAI/MagicFridgeAiApplication.java
@@ -0,0 +1,13 @@
+package dev.java.MagicFridgeAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicFridgeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicFridgeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java/MagicFridgeAI/MagicFridgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.MagicFridgeAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicFridgeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
